3b2b6902badd8e55083b971e915403a3a3aa2c49
Anjelica <ambrosio.anjelica@gmail.com>
2021-02-10 17:26:24 -0800

    content component built

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Title from './title';
+import POPOSList from './POPOSList';
 
 
 function App() {
   return (
     <div className="App">
       <Title />
+      <POPOSList />
     </div>
   );
 }
