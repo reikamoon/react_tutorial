ea6490deefc64315d6f3bd465c38c5337e716969
Anjelica <ambrosio.anjelica@gmail.com>
2021-03-03 14:13:16 -0800

    fixed images issue

--- src/components/POPOSSpace/POPOSSpace.js
+++ src/components/POPOSSpace/POPOSSpace.js
@@ -10,7 +10,7 @@ function POPOSSpace(props) {
     return (
       <div>
         <Link to={`/details/${id}`}>
-            <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
+            <img src={`${process.env.PUBLIC_URL}/images/${image}`} width="300" height="300" alt="Hello" />
         </Link>
         <h1>
             <Link
